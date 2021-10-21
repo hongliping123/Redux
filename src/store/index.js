fa39d9e581a30417f6b21af45c904100675ae238
一只英俊的小马 <851358176@qq.com>
2021-10-21 17:29:59 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+/**
+ * @Author: Administrator 一只英俊的小马
+ * @Date: 2021-10-21
+ */
+import {createStore} from "redux";
+const store = createStore()
---
+++ src/store/reducer.js
@@ -0,0 +1,4 @@
+/**
+  * @Author: Administrator 一只英俊的小马
+  * @Date: 2021-10-21
+*/
